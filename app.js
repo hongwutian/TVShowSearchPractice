c89fb7e3cc19432b52e476fa26ce43a4925cb452
hongwutian <hongwu32@yahoo.com>
2021-02-02 02:49:29 -0800

    finished

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const form = document.querySelector("searchForm")
-form.addEventListener("submit", e => {
+const form = document.querySelector("#searchForm")
+form.addEventListener("submit", async e => {
     e.preventDefault()
-    console.log("submitted ")
+    console.log("submitted")
+    const searchTerm = form.elements.query.value
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config)
+    makgImage(res.data)
+    form.elements.query.value = ''
 })
+
+const makgImage = pictures => {
+    for (let picture of pictures) {
+        if (picture.show.image) {
+        const img = document.createElement('IMG')
+        img.src = picture.show.image.medium
+        document.body.append(img)
+        }
+    }
+}
